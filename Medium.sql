ac6dd9cd10d2f4e7570c7ffa851a84299e868f0d
Yuvaraj Pazhamalai <31355861+yuvarajp90@users.noreply.github.com>
2018-08-19 23:01:25 -0700

    Update Medium.sql

--- Medium.sql
+++ Medium.sql
@@ -18,3 +18,22 @@ when n in (select distinct p from bst) then 'Inner'
 else 'Leaf' end
 from bst
 order by n
+
+--Q3
+--Query the Manhattan Distance between points p1 and p1 and round it to a scale of 4 decimal places
+select round(abs(min(lat_n)-max(lat_n))+abs(min(long_w)-max(long_w)),4) from station;
+                                            
+--Q4
+--Query the euclidean Distance between points p1 and p1 and round it to a scale of 4 decimal places
+select round(sqrt((power((max(lat_n)-min(lat_n)),2) + power((max(long_w)-min(long_w)),2))),4) from station;
+             
+--Q5
+--Generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+                                            
+select case when b.grade >7 then a.name else null end as name,b.grade,a.marks
+from students a
+left join
+grades b
+on a.marks between b.min_mark and b.max_mark
+order by grade desc, name asc, marks asc
+
