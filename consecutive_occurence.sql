6e92081236fd7a7801a23927050c6f7eeb772356
Yuvaraj Pazhamalai <31355861+yuvarajp90@users.noreply.github.com>
2018-08-20 11:12:03 -0700

    Create consecutive_occurence.sql

---
+++ consecutive_occurence.sql
@@ -0,0 +1,15 @@
+--3 consecutive occurence
+--depending on the number of repetition, that many number of cross joins would be required
+
+SELECT DISTINCT
+    l1.Num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+;
